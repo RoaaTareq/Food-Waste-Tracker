7d02390902ccae05b480276963149e1d8e44a44a
Roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-03-24 23:07:06 +0300

    css

--- src/Components/Home/Topsection.jsx
+++ src/Components/Home/Topsection.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import "../../App.css";
 import "../../assets/styles/components/Topsection.css";
-import Tracking from "../../assets/Images/tracker.jpg";
+import Tracking from "../../assets/Images/boxfood.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Topsection = () => {
     return (
         <section className="Main-section">
             <div className="container">
-                <div className="row justify-content-between">
+                <div className="row justify-content-between align-items-center">
                     <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <h1 className="main-title">Food Waste Tracker</h1>
+                        <h1 className="main-title">1st Tracker System For Food</h1>
                         <p className="main-text">
-                        The Food Waste Tracker revolutionizes waste reduction, empowering users with actionable insights for a greener, more sustainable future.
+                        Introducing our game-changing Food Waste Tracker, the ultimate tool for eco-conscious consumers and businesses in the Mean region. Say goodbye to wasted resources and hello to efficiency with our intuitive system. Track, analyze, and reduce food waste effortlessly, saving money and the planet simultaneously. Join the movement towards a greener Mean – because when it comes to sustainability, every scrap counts.
                         </p>
-                        {/* <h2 className="sub-fact" >6 FACTS</h2>
-                        <div className="d-flex  justify-content-between card-group">
-                            <div className="card-info">2</div>
-                            <div className="card-info">3</div>
-                            <div className="card-info">4</div>
-                            <div className="card-info">5</div>
-                            <div className="card-info">6</div>
+                       
+                        {/* <div className="d-flex  justify-content-between card-group">
+                            <ul className="list-goals">
+                                <li> <span><FontAwesomeIcon icon={faCheck} /></span>Community Impact</li>
+                                <li> <span><FontAwesomeIcon icon={faCheck} /></span>Awareness</li>
+                                <li><span><FontAwesomeIcon icon={faCheck} /></span>Efficiency</li>
+                            </ul>
+                            
                         </div> */}
                     </div>
                     <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 d-flex justify-content-end">
--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
   return (
     <div>
       <Topsection/>
-    <About/>
-    <Team/>
+    {/* <About/>
+    <Team/> */}
     </div>
   );
 };
