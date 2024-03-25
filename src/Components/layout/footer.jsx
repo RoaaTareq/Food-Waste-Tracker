288b22da49dec70329b78e18ce9e9e64381bace4
Roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-03-25 22:13:47 +0300

    Change

--- src/Components/layout/footer.jsx
+++ src/Components/layout/footer.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import "../../App.css";
+import { useTranslation } from 'react-i18next';
 import "../../assets/styles/components/footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
+    const { t , i18n } = useTranslation();
+    const mainrtl = i18n.language === 'ar';
     return (
-        <section className="footer-section">
+        <section className={ mainrtl ? 'mainrtl' : ''}>
             <hr />
             <div className="container">
                 <div className="row align-items-center ">
                     <div className="col-xl-5 col-lg-4 col-md-12 col-sm-12 col-xs-12">
-                        <h5 className="title-footer">Food Waste Tracker</h5>
-                        <p className="about-text">The Food Waste Tracker revolutionizes waste reduction, empowering users with actionable insights for a greener, more sustainable future.</p>
+                    <h1 className="main-title">Food <sub>Limit</sub></h1>
+                    <p className="main-text">
+                       {t('text-main')}
+                        </p>
                     </div>
                     <div className="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-xs-12">
-                        <h5 className="title-footer">Useful link</h5>
+                        <h5 className="title-footer">{t('useful')} </h5>
                         <ul className="useful-list">
-                            <li className="usful-item">Home</li>
-                            <li className="usful-item">Career</li>
-                            <li className="usful-item">About</li>
+                            <li className="usful-item">{t('home')}</li>
+                            <li className="usful-item">{t('about')}</li>
+                            <li className="usful-item">{t('career')}</li>
                         </ul>
                     </div>
                     <div className="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-xs-12">
                         <div>
-                            <h5 className="title-footer">Follow US</h5>
+                            <h5 className="title-footer">{t('Follow')}</h5>
                             <ul className="socialmedia-list">
                                 <li className="item">
                                     <FontAwesomeIcon icon={faFacebook} />
