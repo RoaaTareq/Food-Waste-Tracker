0114d683ee15a8bf0e8fc302257795e1d70c7e14
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-09-10 15:36:22 +0300

    Top section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TopSection from './Topsection'
 import KeyFeatures from './KeyFetures'
+import HowAre from'./HowAreYou'
 
 function Home(){
     return(
         <>
         <TopSection/>
         <KeyFeatures/>
+        <HowAre/>
         </>
     )
 }
---
+++ src/Pages/Home/HowAreYou.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styles from './CSS/Home.module.css'
+import Card from '../../Components/Card/Card'
+import Value from '../../assets/Images/Value.png'
+import Mission from '../../assets/Images/Mission.png'
+import Vision from '../../assets/Images/Vision.png'
+
+function Home (){
+    return(
+    <section className={styles['about']}>
+    <div className="container">
+        <h1 className='title'> How Are You ?</h1>
+    <div className="row align-items-center ">
+        <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+        <Card
+        title="Reports for cost savings and sustainability."
+        image={ Value}
+        
+        className="custom-card "
+      />
+      
+        </div>
+        <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+        <Card
+        title="Food waste monitoring and evalutaing"
+        image={Mission}
+    
+        className="custom-card "
+      />
+        </div>
+        <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+        <Card
+        title="Real-time tracking and data analytics"
+        image={Vision}
+    
+        className="custom-card"
+      />
+        </div>
+    </div>
+    </div>
+</section>
+    )
+}
+
+export default Home
